c78fa1ad863758b8146e0b59149e339070884690
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-06-22 07:15:02 +0200

    Updates tests

--- test/templates-test.js
+++ test/templates-test.js
@@ -3,13 +3,11 @@
 const tap = require('tap')
 const templates = require('../templates/templates.json')
 
-tap.equal(Object.keys(templates).length, 7, 'There are 7 different templates')
+tap.equal(Object.keys(templates).length, 6, 'There are 6 different templates')
 
 tap.ok(templates.avslag, 'There is a template for avslag')
 
 tap.ok(templates.innvilget, 'There is a template for innvilget')
-
-tap.ok(templates['doble-kontakter'], 'There is a template for doble-kontakter')
 
 tap.ok(templates['midlertidig-annen'], 'There is a template for midlertidig-annen')
 
