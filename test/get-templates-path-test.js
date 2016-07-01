f71559e0198daf48ec54fd4c926bbd0fbbfe8aa1
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-07-01 12:48:41 +0200

    Adds tests for files

--- test/get-templates-path-test.js
+++ test/get-templates-path-test.js
@@ -1,7 +1,8 @@
 'use strict'
 
-var tap = require('tap')
-var getTemplatePath = require('../index')
+const tap = require('tap')
+const isFile = require('is-file')
+const getTemplatePath = require('../index')
 
 tap.ok(getTemplatePath('avslag'), 'It returns path if template exists')
 
@@ -20,3 +21,15 @@ tap.throws(
   {message: 'Template not found'},
   'Throws if template does not exist'
 )
+
+tap.equal(isFile(getTemplatePath('avslag')), true, 'Template for avslag is a file')
+
+tap.equal(isFile(getTemplatePath('innvilget')), true, 'Template for innvilget is a file')
+
+tap.equal(isFile(getTemplatePath('midlertidig-annen')), true, 'Template for midlertidig-annen is a file')
+
+tap.equal(isFile(getTemplatePath('midlertidig-delt')), true, 'Template for midlertidig-delt is a file')
+
+tap.equal(isFile(getTemplatePath('midlertidig-hybel')), true, 'Template for midlertidig-hybel is a file')
+
+tap.equal(isFile(getTemplatePath('midlertidig-manuell')), true, 'Template for midlertidig-manuell is a file')
